db566984d3844754a86b1acba56ac1ea3924b1d8
Helior Colorado <me@helior.info>
2017-06-23 20:26:12 -0700

    User input all figured out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import getParameterByName from './utils/getParameterByName';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import logo from './logo.svg';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  getSearchUrl() {
+    return getParameterByName('searchUrl');
+  }
+  getGoogleApiKey() {
+    return getParameterByName('googleApiKey');
+  }
+
   render() {
     return (
-      <Container>
-        <Row>
+      <Container fluid>
+        <Row className="user-input">
           <Col>
-            <Form>
+            <Form inline className="m-2">
               <FormGroup>
-                <Label for="searchUrl">Search URL</Label>
-                <Input type="text" name="searchUrl" id="searchUrl" placeholder="http://example.com/?q=123" />
+                <Label for="searchUrl" hidden>Search URL</Label>
+                <Input type="text" name="searchUrl" id="searchUrl" placeholder="Search URL" defaultValue={this.getSearchUrl()} />
               </FormGroup>
+              {' '}
+              <FormGroup>
+                <Label for="googleApiKey" hidden>Google API Key</Label>
+                <Input type="text" name="googleApiKey" id="googleApiKey" placeholder="Google API Key" defaultValue={this.getGoogleApiKey()} />
+              </FormGroup>
+              {' '}
+              <Button>Submit</Button>
             </Form>
           </Col>
-          </Row>
+        </Row>
       </Container>
     );
   }
