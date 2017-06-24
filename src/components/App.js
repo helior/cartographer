706ddd8604643c0fb95a42eb5ad747dbb4b7d3b5
Helior Colorado <me@helior.info>
2017-06-24 14:34:57 -0700

    A bit of shuffling of files

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import getParameterByName from '../lib/getParameterByName';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import UserInput from './UserInput';
+import Map from './Map';
+import Placeholder from './Placeholder';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchUrl: getParameterByName('searchUrl'),
+      googleApiKey: getParameterByName('googleApiKey'),
+      isLoading: false,
+      markers: []
+    }
+  }
+
+  hasRequiredInput() {
+    return this.state.searchUrl && this.state.googleApiKey;
+  }
+
+  hasMarkers() {
+    return this.state.markers.length > 0;
+  }
+
+  componentDidMount() {
+    if (this.hasRequiredInput()) {
+      this.setState({isLoading: true});
+      // HTTP request...
+      // then( set marker state)
+    }
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row className="input-row">
+          <Col>
+            <UserInput searchUrl={this.state.searchUrl} googleApiKey={this.state.googleApiKey} />
+          </Col>
+        </Row>
+        <Row className="map-row">
+          <Col>
+           {this.hasRequiredInput() && this.hasMarkers() ?
+           <Map apiKey={this.state.googleApiKey} markers={this.state.markers} /> :
+           <Placeholder isLoading={this.state.isLoading} />}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Jumbotron } from 'reactstrap';
 
 class Placeholder extends React.Component {
+  static defaultProps = {
+    isLoading: false
+  }
+
+  componetnWillReceiveProps() {
+    console.log(this.props);
+  }
+
   render() {
     return (
       <Jumbotron className="m-5">
         <p className="lead text-center">Enter the Search URL <em>and</em> your Google API Key.</p>
+        <p className="lead text-center">{this.props.isLoading ? 'Loading...' : '👻'}</p>
       </Jumbotron>
     )
   }
